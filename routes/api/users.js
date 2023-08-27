44fad4bc490142ed8ce6b473c2211f06af9cf938
Peter Lauer <lauerpeter11@gmail.com>
2023-08-27 15:10:51 -0500

    added api routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,19 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers').userController;
 
+// Get all users
+router.get('/', userController.getAllUsers);
 
+// Get a single user by ID
+router.get('/:id', userController.getUserById);
+
+// Create a new user
+router.post('/', userController.createUser);
+
+// Update a user by ID
+router.put('/:id', userController.updateUser);
+
+// Delete a user by ID
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
